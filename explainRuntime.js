64ff0e26d91ab9dde6d809f9a1826e976c46e7e9
hanguyen <hanguyen.40091@gmail.com>
2021-04-19 07:22:30 +0700

    execution context

--- explainRuntime.js
+++ explainRuntime.js
@@ -1,20 +1,8 @@
-// console.log("1")
-// setTimeout(() => {
-//     console.log("set time out 1")
-// }, 1);
-// setTimeout(() => {
-//     console.log("set time out 2")
-// }, 0);
-// const a = Promise.resolve("promise")
-// a.then((data) => {
-//     console.log(data)
-// })
-// console.log("2")
+let a = 5
+let b = 1
 
-
-setTimeout(() => console.log("timeout"));
-
-Promise.resolve()
-    .then(() => console.log("promise"));
-
-console.log("code");
+a = a ^ b
+b = b ^ a
+a = a ^ b
+console.log("a: ",a)
+console.log("b: ",b)
